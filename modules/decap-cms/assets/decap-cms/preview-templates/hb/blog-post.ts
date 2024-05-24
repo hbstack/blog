45cb68426ef255748ef5412437639df09aaa863b
razonyang <razonyang@gmail.com>
2024-05-24 11:11:38 +0800

    chore: format codes

--- modules/decap-cms/assets/decap-cms/preview-templates/hb/blog-post.ts
+++ modules/decap-cms/assets/decap-cms/preview-templates/hb/blog-post.ts
@@ -1,110 +1,110 @@
 export default createClass({
   renderTaxonomy: function (item, index) {
     return h(
-      "span",
+      'span',
       {
         key: index,
-        className: "hb-blog-post-taxonomy-meta",
+        className: 'hb-blog-post-taxonomy-meta'
       },
       h(
-        "span",
+        'span',
         {
           className:
-            "hb-blog-post-taxonomy hb-blog-post-taxonomy-category badge bg-secondary text-decoration-none fw-normal ms-1",
+            'hb-blog-post-taxonomy hb-blog-post-taxonomy-category badge bg-secondary text-decoration-none fw-normal ms-1'
         },
-        item,
-      ),
-    );
+        item
+      )
+    )
   },
   renderAuthor: function (name, index) {
     return h(
-      "span",
+      'span',
       { key: index },
       h(
-        "span",
+        'span',
         {
           className:
-            "hb-blog-author-img border rounded-circle px-1 text-bg-secondary",
-          title: name,
+            'hb-blog-author-img border rounded-circle px-1 text-bg-secondary',
+          title: name
         },
-        name ? name.substr(0, 1).toUpperCase() : "",
-      ),
-    );
+        name !== '' ? name.substr(0, 1).toUpperCase() : ''
+      )
+    )
   },
   renderImage: function (img, index) {
-    if (!img) {
-      return;
+    if (img == null) {
+      return
     }
 
-    return h("img", {
+    return h('img', {
       key: index,
-      className: "border",
+      className: 'border',
       style: {
-        height: "200px",
-        minWidth: "60px",
+        height: '200px',
+        minWidth: '60px'
       },
-      src: this.props.getAsset(img).toString(),
-    });
+      src: this.props.getAsset(img).toString()
+    })
   },
   render: function () {
-    const authors = this.props.entry.getIn(["data", "authors"]);
-    const categories = this.props.entry.getIn(["data", "categories"]);
-    const images = this.props.entry.getIn(["data", "images"]);
-    const series = this.props.entry.getIn(["data", "series"]);
-    const tags = this.props.entry.getIn(["data", "tags"]);
+    const authors = this.props.entry.getIn(['data', 'authors'])
+    const categories = this.props.entry.getIn(['data', 'categories'])
+    const images = this.props.entry.getIn(['data', 'images'])
+    const series = this.props.entry.getIn(['data', 'series'])
+    const tags = this.props.entry.getIn(['data', 'tags'])
 
     return h(
-      "div",
+      'div',
       {
-        className: "hb-blog-post d-flex flex-column m-3",
+        className: 'hb-blog-post d-flex flex-column m-3'
       },
       h(
-        "h1",
+        'h1',
         {
-          className: "hb-blog-post-title",
+          className: 'hb-blog-post-title'
         },
-        this.props.entry.getIn(["data", "title"]),
+        this.props.entry.getIn(['data', 'title'])
       ),
       h(
-        "div",
+        'div',
         {
-          className: "hb-blog-post-meta",
+          className: 'hb-blog-post-meta'
         },
-        authors ? authors.map(this.renderAuthor) : null,
+        authors != null ? authors.map(this.renderAuthor) : null,
         h(
-          "div",
+          'div',
           {
-            className: "hb-blog-post-date",
+            className: 'hb-blog-post-date'
           },
-          this.props.entry.getIn(["data", "date"]).toString(),
+          this.props.entry.getIn(['data', 'date']).toString()
         ),
-        series ? series.map(this.renderTaxonomy) : null,
-        categories ? categories.map(this.renderTaxonomy) : null,
-        tags ? tags.map(this.renderTaxonomy) : null,
+        series != null ? series.map(this.renderTaxonomy) : null,
+        categories != null ? categories.map(this.renderTaxonomy) : null,
+        tags != null ? tags.map(this.renderTaxonomy) : null
       ),
       h(
-        "div",
+        'div',
         {
-          className: "hb-blog-post-desc lead mb-0",
+          className: 'hb-blog-post-desc lead mb-0'
         },
-        this.props.entry.getIn(["data", "description"]),
+        this.props.entry.getIn(['data', 'description'])
       ),
-      images
+      images != null
         ? h(
-            "div",
-            {
-              className: "hb-blog-post-img d-flex gap-1",
-            },
-            images.map(this.renderImage),
-          )
+          'div',
+          {
+            className: 'hb-blog-post-img d-flex gap-1'
+          },
+          images.map(this.renderImage)
+        )
         : null,
       h(
-        "div",
+        'div',
         {
-          className: "hb-blog-post-content",
+          className: 'hb-blog-post-content'
         },
-        this.props.widgetFor("body"),
-      ),
-    );
-  },
-});
+        this.props.widgetFor('body')
+      )
+    )
+  }
+})
